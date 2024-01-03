ecfec4600e334046fc723a6e690e4384314ab77d
amz_office <m.a.zhang@outlook.com>
2024-01-03 16:56:09 +0000

    add generic meta that is used for pixels & behaviour analysis

---
+++ common_utils/generic_meta.py
@@ -0,0 +1,69 @@
+"""
+generic meta setup for data analysis
+"""
+
+import time
+
+'''
+mice
+'''
+# tests
+msc = [
+    "HFR20",
+]
+tom = [
+    "TF061",
+]
+
+# estrous
+oes_cohort_1 = [
+    "ESCN00",
+    "ESCN02",
+    "ESCN03",
+]
+
+# vision in darkness
+vd_cohort_1 = [
+    "WDAN00",
+    "WDAN01",
+]
+vd_cohort_2 = [
+    "WDAN03",
+    "WDAN04",
+]
+vd_cohort_3 = [
+    "WDAN07",
+    "WDAN06",
+    "WDAN05",
+]
+
+'''
+directories
+'''
+# data
+ardbeg = "~/rochefortlab/arthur/"
+storage = "~/storage/data/"
+interim = "~/interim/"
+server_w = "~/w/arthur/"
+server_v = "~/v/arthur/"
+
+# meta
+oestrus_meta = "/home/amz/w/arthur/JSONs"
+
+# histology
+hist_dir = "/home/amz/rochefortlab/arthur/histology/"
+
+# figures
+fig_dir = f"/home/amz/w/arthur/plots/behaviour/"
+crap_fig_dir = f"/home/amz/Pictures/crappy_plots/"
+# TODO jan 3 mice is not defined, how to preset prefix here?
+#fig_prefix = f"{time.strftime('%Y%m%d')}_{'_'.join(mice)}_"
+
+# testing
+test_data = "/home/amz/running_data/npx"
+test_interim = "/home/amz/running_data/npx/interim"
+
+'''
+misc
+'''
+session_date_fmt = "%Y%m%d" # datetime format yyyymmdd
