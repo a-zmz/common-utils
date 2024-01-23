dc8eab28692bb8876b1aeaf4871117348638be60
amz_office <m.a.zhang@outlook.com>
2024-01-23 17:18:52 +0000

    move file utils in analysis to here

--- common_utils/file_utils.py
+++ common_utils/file_utils.py
@@ -2,6 +2,9 @@
 This contains utility functions to handle files.
 """
 import os
+import operator
+import json
+import tempfile
 from pathlib import Path
 import pickle
 import shutil
@@ -77,3 +80,67 @@ def copy_files(source: str, dests: list, basename: str) -> None:
             else:
                 #print("already copied, next")
                 continue
+
+
+def read_json(path: str, names: list) -> None:
+    path = file_utils.make_path(path)
+    for name in names:
+        metadata = pd.read_json(path / f"{name}.json")
+        print(f"\n{name}\n {metadata}")
+
+
+def write_json(metadata: dict, file: str) -> None:
+    with file.open(mode="w") as fd:  # opens file for writing
+        json.dump(metadata, fd)
+        print(f"Metadata saved in {file}")
+
+
+def save_behaviour_json(meta_dir: str, mice: list) -> None:
+    meta_dir = file_utils.make_path(meta_dir)
+    for mouse in mice:
+        meta = []
+        file = meta_dir / f"{mouse}.json"
+        # input date
+        if input(f"\nInput meta from previous date? ") == "yes":
+            date = input("date(yyyymmdd): ")
+        else:
+            date = datetime.now().strftime("%Y%m%d") # save date & time
+            metadata = {
+                "date": date, # save date & time
+                "bodyweight (g)": input(f"Bodyweight of {mouse}: "),
+                "note": input("Note: "),
+            }
+
+            meta.append(metadata)
+
+            if file.exists():
+                # get # old meta
+                old_meta = json.load(open(file, mode="r"))
+                # extend old meta
+                old_meta.extend(meta)
+                meta = old_meta
+                print("Merged some old data.")
+            else:
+                meta = meta
+
+            # save data to json
+            try:
+                write_json(meta, file)
+            except FileNotFoundError:
+                print("File does not exist.")
+                write_json(meta, Path(tempfile.gettempdir()) / f"{mouse}.json")
+            except Exception as e: # pylint: # disable=broad-except
+                write_json(meta, Path(tempfile.gettempdir()) / f"{mouse}.json")
+                print(f"Exception raised while saving: {type(e)}")
+                print("Please report this.")
+ 
+
+def sort_json_by_date(meta_dir: str, mice: list, output_dir: str = None) -> None:
+    for mouse in mice: # load meta
+        meta = json.load(open(meta_dir / f"{mouse}.json", "r"))
+        # sort by date
+        meta.sort(key=operator.itemgetter("date"))
+        # save file
+        if output_dir == None:
+            output_dir = meta_dir
+        write_json(meta, output_dir / f"{mouse}.json")
