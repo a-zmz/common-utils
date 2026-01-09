6ecffdc29f0bf5106add01ab7dbf9e01929a0a8a
amz_office <m.a.zhang@outlook.com>
2026-01-09 12:36:47 +0000

    add more colourblind save palettes

--- common_utils/colour_utils.py
+++ common_utils/colour_utils.py
@@ -128,6 +128,18 @@ cmap_names = ["cubehelix", "cmo.thermal_r_i", "cmo.oxy", "cmo.dense_i",
 """
 
 # >>> colourblind friendly colours >>>
-categorical = ["#CC6677", "#332288", "#DDCC77", "#117733", "#88CCEE", "#882255",
-               "#44AA99", "#999933", "#AA4499"]
+# NOTE: by paul tol from https://sronpersonalpages.nl/~pault/
+bright = ["#4477AA", "#EE6677", "#228833", "#CCBB44", "#66CCEE", "#AA3377",
+          "#BBBBBB"]
+high_contrast = ["#004488", "#DDAA33", "#BB5566"]
+med_contrast = ["#6699CC", "#004488", "#EECC66", "#994455", "#997700",
+                "#EE99AA"]
+vibrant = ["#EE7733", "#0077BB", "#33BBEE", "#EE3377", "#CC3311", "#009988",
+           "#BBBBBB"]
+muted = ["#CC6677", "#332288", "#DDCC77", "#117733", "#88CCEE", "#882255",
+         "#44AA99", "#999933", "#AA4499"]
+pale = ["#BBCCEE", "#CCEEFF", "#CCDDAA", "#EEEEBB", "#FFCCCC", "#DDDDDD"]
+darker ["#222255", "#225555", "#225522", "#666633", "#663333", "#555555"]
+lighter = ["#77AADD", "#EE8866", "#EEDD88", "#FFAABB", "#99DDFF", "#44BB99",
+           "#BBCC33", "#AAAA00", "#DDDDDD"]
 # <<< colourblind friendly colours <<<
